690a39a89bd98f64a6d23fd07d2fba2318c21305
simejisan <letam.tamle@gmail.com>
2021-07-23 20:51:18 +0700

    fix ui

--- src/components/FormResult.js
+++ src/components/FormResult.js
@@ -40,7 +40,6 @@ const FormResult = (props) => {
   // });
   // const [checkClicked, setCheckClicked] = useState(false);
 
-
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -86,8 +85,8 @@ const FormResult = (props) => {
     let bgColor = "#ffd2da";
 
     // setBgColor(bgColor2);
-    console.log("focus: "+ bgColor2);
-    console.log("normal: "+ bgColor);
+    console.log("focus: " + bgColor2);
+    console.log("normal: " + bgColor);
 
     // e.target.style.backgroundColor = checkClicked? bgColor:bgColor2;
     // console.log(e.target.style.backgroundColor);
@@ -104,159 +103,162 @@ const FormResult = (props) => {
   return (
     <div className="form-container">
       <div className="d-flex flex-column">
-        {/* <div className="result-form">
-          <div className="text">Kết quả của bạn là </div>
-          {result && <div className="box-result">{result}</div>}
-        </div> */}
         <div className="fb-form justify-content-center">
-          <img
+          {/* <img
             src="/images/wow.png"
             className="img-fluid px-3 fb-image"
             alt="wow mascos"
-          />
-          <div class="card fb-card">
-            <div className="fb-card-header p-3 mb-3">Đánh giá Sogienator</div>
-            <form
-              className="d-flex flex-column bd-highlight form-feedback"
-              onSubmit={(e) => handleSubmit(e)}
-            >
-              <div className="fb-content px-2 pt-3  text-center">
-                Trải nghiệm về kết quả
-              </div>
-              <div className="list-feedback px-3 pt-2 mr-1">
-                <div
-                  className="first-q"
-                  onClick={(e) => handleClick(e)}
-                  id="accuracy"
-                  value={data.accuracy}
-                >
-                  <div className="p-3 bd-highlight list-feedback-item " id="1">
-                    Không chính xác
-                  </div>
-                  <div className=" p-3 bd-highlight list-feedback-item " id="2">
-                    Gần chính xác
-                    <Tooltip
-                      title="Sogienator đã tính toán đúng một phần nhưng chưa đầy đủ"
-                      position="top"
-                      trigger="mouseenter"
-                      animation="fade"
-                      theme="dark"
-                      className="fb-tooltip"
-                      size="regular"
-                    >
-                      <Button
-                        buttonSize="btn--esmall"
-                        buttonStyle="btn--tooltip"
-                        data-toggle="tooltip"
-                        data-placement="right"
-                        title="Tooltip on right"
-                      >
-                        ?
-                      </Button>
-                    </Tooltip>
-                  </div>
-                  <div className="p-3 bd-highlight list-feedback-item " id="3">
-                    Chính xác
-                  </div>
-                  <div className="p-3 bd-highlight list-feedback-item " id="4">
-                    Có kết quả nhưng cảm thấy không giống với bản thân
-                    <Tooltip
-                      title="Nếu bạn chưa từng xác định trước"
-                      position="top"
-                      trigger="mouseenter"
-                    >
-                      <Button
-                        buttonSize="btn--esmall"
-                        buttonStyle="btn--tooltip"
-                        data-toggle="tooltip"
-                        data-placement="right"
-                        title="Tooltip on right"
-                      >
-                        ?
-                      </Button>
-                    </Tooltip>
-                  </div>
-                  <div className="p-3 bd-highlight list-feedback-item " id="5">
-                    Có kết quả và cảm thấy giống với bản thân
-                    <Tooltip
-                      title="Nếu bạn từng xác định trước"
-                      position="top"
-                      trigger="mouseenter"
-                    >
-                      <Button
-                        buttonSize="btn--esmall"
-                        buttonStyle="btn--tooltip"
-                        data-toggle="tooltip"
-                        data-placement="right"
-                        title="Tooltip on right"
-                      >
-                        ?
-                      </Button>
-                    </Tooltip>
-                  </div>
-                </div>
-              </div>
-              <div className="first-q">
-                <div className="fb-content px-2 pt-3 text-center">
-                  Tuổi của bạn{" "}
-                </div>
-                <div
-                  className="list-feedback p-4"
-                  onClick={(e) => handleClick(e)}
-                  id="age"
-                  value={data.age}
-                >
-                  <div className="p-3 bd-highlight list-feedback-item " id="1">
-                    Dưới 15
-                  </div>
-                  <div className=" p-3 bd-highlight list-feedback-item " id="2">
-                    Trong độ tuổi từ 15 đến 20
-                  </div>
-                  <div className="p-3 bd-highlight list-feedback-item " id="3">
-                    Trong độ tuổi từ 21 đến 30
-                  </div>
-                  <div className="p-3 bd-highlight list-feedback-item " id="4">
-                    Trên 30
-                  </div>
-                </div>
-              </div>
+          /> */}
+          {/* <div class="card fb-card"> */}
+          <div className="fb-card-header p-3">Đánh giá Sogienator</div>
+          <form
+            className="d-flex flex-column bd-highlight form-feedback"
+            onSubmit={(e) => handleSubmit(e)}
+          >
+            <div className="fb-content px-2 pt-3 text-center">
+              1, Trải nghiệm về kết quả
+            </div>
+            <div className="list-feedback px-3 pt-4 mr-1">
               <div
                 className="first-q"
-                onChange={(e) => handleClick(e)}
+                onClick={(e) => handleClick(e)}
+                id="accuracy"
+                value={data.accuracy}
+              >
+                <div className="p-3 bd-highlight list-feedback-item " id="1">
+                  Không chính xác
+                </div>
+                <div className=" p-3 bd-highlight list-feedback-item " id="2">
+                  Gần chính xác
+                  <Tooltip
+                    title="Sogienator đã tính toán đúng một phần nhưng chưa đầy đủ"
+                    position="top"
+                    trigger="mouseenter"
+                    animation="fade"
+                    theme="dark"
+                    className="fb-tooltip"
+                    size="regular"
+                  >
+                    <Button
+                      buttonSize="btn--esmall"
+                      buttonStyle="btn--tooltip"
+                      data-toggle="tooltip"
+                      data-placement="right"
+                      title="Tooltip on right"
+                    >
+                      ?
+                    </Button>
+                  </Tooltip>
+                </div>
+                <div className="p-3 bd-highlight list-feedback-item " id="3">
+                  Chính xác
+                </div>
+                <div className="p-3 bd-highlight list-feedback-item " id="4">
+                  <div className="text-center">
+                    Có kết quả nhưng cảm thấy không giống với bản thân
+                  </div>
+                  <Tooltip
+                    title="Nếu bạn chưa từng xác định trước"
+                    position="top"
+                    trigger="mouseenter"
+                  >
+                    <Button
+                      buttonSize="btn--esmall"
+                      buttonStyle="btn--tooltip"
+                      data-toggle="tooltip"
+                      data-placement="right"
+                      title="Tooltip on right"
+                    >
+                      ?
+                    </Button>
+                  </Tooltip>
+                </div>
+                <div className="p-3 bd-highlight list-feedback-item " id="5">
+                  <div className="text-center">
+                    Có kết quả và cảm thấy giống với bản thân
+                  </div>
+
+                  <Tooltip
+                    title="Nếu bạn từng xác định trước"
+                    position="top"
+                    trigger="mouseenter"
+                  >
+                    <Button
+                      buttonSize="btn--esmall"
+                      buttonStyle="btn--tooltip"
+                      data-toggle="tooltip"
+                      data-placement="right"
+                      title="Tooltip on right"
+                    >
+                      ?
+                    </Button>
+                  </Tooltip>
+                </div>
+              </div>
+            </div>
+            <div className="fb-content px-2 pt-3 text-center">
+              2, Tuổi của bạn{" "}
+            </div>
+            {/* <div className="list-feedback px-3 pt-4 mr-1"> */}
+            {/* <div className="first-q"> */}
+
+            <div
+              className="first-q p-4"
+              onClick={(e) => handleClick(e)}
+              id="age"
+              value={data.age}
+            >
+              <div className="p-3 bd-highlight list-feedback-item " id="1">
+                Dưới 15
+              </div>
+              <div className=" p-3 bd-highlight list-feedback-item " id="2">
+                Trong độ tuổi từ 15 đến 20
+              </div>
+              <div className="p-3 bd-highlight list-feedback-item " id="3">
+                Trong độ tuổi từ 21 đến 30
+              </div>
+              <div className="p-3 bd-highlight list-feedback-item " id="4">
+                Trên 30
+              </div>
+            </div>
+            {/* </div> */}
+            {/* </div> */}
+            <div className="fb-content px-4 pb-4 text-center">
+              3, Bạn còn điều gì muốn bày tỏ hoặc góp ý chi tiết thêm với Vy An
+              không?{" "}
+            </div>
+            <div
+              className="first-q"
+              onChange={(e) => handleClick(e)}
+              id="additional"
+              value={data.additional}
+            >
+              {/* <div className="list-feedback p-4"> */}
+              <div
+                className="p-3 bd-highlight fb-text "
+                onClick={(e) => handleClick(e)}
                 id="additional"
                 value={data.additional}
               >
-                <div className="fb-content px-4 pt-2 text-center">
-                  Bạn còn điều gì muốn bày tỏ hoặc góp ý chi tiết thêm với Vy An
-                  không?{" "}
-                </div>
-                <div className="list-feedback p-4">
-                  <div
-                    className="p-3 bd-highlight list-feedback-item fb-text "
-                    onClick={(e) => handleClick(e)}
-                    id="additional"
-                    value={data.additional}
-                  >
-                    <textarea
-                      className="fb-textarea"
-                      row="4"
-                      placeholder="Câu trả lời của bạn"
-                      type="text"
-                    ></textarea>
-                  </div>
-                </div>
+                <textarea
+                  className="fb-textarea"
+                  row="6"
+                  placeholder="Câu trả lời của bạn"
+                  type="text"
+                ></textarea>
               </div>
-
-              <div className="list-feedback-item pb-3 fb-btn-round ">
-                <button className="p-3 fb-btn " type="submit">
-                  Gửi
-                </button>
-              </div>
-            </form>
-          </div>
+            </div>
+            {/* </div> */}
+            <div className="text-center pt-3 pb-3 ">
+              <button className="p-3 fb-btn " type="submit">
+                GỬI
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 export default FormResult;
