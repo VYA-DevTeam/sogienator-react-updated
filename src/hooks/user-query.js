3900eb2ea81f2732539d977c3c8a5eb295804261
Cumbercubie <ritoalpha1843@gmail.com>
2022-05-16 03:31:40 +0700

    Update query hook

--- src/hooks/user-query.js
+++ src/hooks/user-query.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-function useQuery() {
+export default function useQuery() {
   const { search } = useLocation();
 
   return React.useMemo(() => new URLSearchParams(search), [search]);
